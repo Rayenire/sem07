11eef590f072bc1e90297eb1bf39b50b4d6c0c5a
Rayenire <aeristarg@gmail.com>
2022-07-14 20:24:32 +0300

    Add home01

---
+++ home01/Program.cs
@@ -0,0 +1,21 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[ ] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-100, 101);
+    Console.Write(array[i] + " ");
+}
+
+int count = 0; 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine(); 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
