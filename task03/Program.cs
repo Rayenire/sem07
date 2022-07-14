f1eabd545b70d3339877653baae99d63a76d0dc2
Rayenire <aeristarg@gmail.com>
2022-07-14 20:25:05 +0300

    Correct 1 line

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,6 +1,6 @@
 ﻿int[, ] CreateMatr(int n, int m)
 {
-    int[, ] matrix = new int[n ,m];
+    int[, ] matrix = new int[n, m];
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
